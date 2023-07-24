37c27afd9cdbf2148ade79120f0c3a0afb3ff7b2
Shatha Al-Bajly <shathaalbajly@gmail.com>
2023-07-24 15:30:53 +0300

    fixes some bugs

--- js/scripts.js
+++ js/scripts.js
@@ -22,77 +22,7 @@ var authors = [
   'Author 12 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem eos ab voluptate nihil quibusdam ullam odio, minima magnam accusamus illum nostrum rem, tempora facere autem ut praesentium laboriosam eaque amet.', 
 
 ];
-const positions = [{
-    top: 0,
-    left: 0
-}, {
-    top: 25,
-    left: 70
-}, {
-    top: 35,
-    left: 85
-}, {
-    top: 65,
-    left: 20
-}, {
-    top: 57,
-    left: 130
-}, {
-    top: 70,
-    left: 105
-}, {
-    top: 10,
-    left: 155
-}, {
-    top: 72,
-    left: 200
-}, {
-    top: 40,
-    left: 235
-}, {
-    top: 25,
-    left: 270
-}, {
-    top: 70,
-    left: 310
-}, {
-    top: 15,
-    left: 330
-}, {
-    top: 40,
-    left: 390
-}, {
-    top: 10,
-    left: 450
-}, {
-    top: 60,
-    left: 440
-}, {
-    top: 50,
-    left: 500
-}, {
-    top: 10,
-    left: 540
-}, {
-    top: 60,
-    left: 600
-}, {
-    top: 20,
-    left: 530
-}, {
-    top: 70,
-    left: 575
-}, {
-    top: 5,
-    left: 630
-}],
-    imagesDom = [],
-    zIndex = 1,
-    scrollSpeed = 1.6,
-    scrollSpeedDefault = 1.6,
-    scrollTimeout = null,
 
-    toleft = 75;
 const wrapper = document.querySelector('.wrapper');
 const scrollable = document.querySelector('.scrollable');
 const caption2 = document.querySelector('nav p')
@@ -117,14 +47,14 @@ const imagesCount = images.length;
 function closeIntro() {
     var t = document.querySelector(".intro")
         , e = document.querySelector("main");
-    tl.to(t, 1, {
+    tl.to(t, 4, {
         opacity: 0,
         ease: "power1.easeOut",
         onComplete: function () {
             t.remove()
         }
     });
-    tl.to(e, 1, {
+    tl.to(e, 3, {
         opacity: 1,
         ease: "power1.easeInOut"
     });
@@ -183,7 +113,7 @@ for (var i = 0; i < images.length; i++) {
 
 
 }
-gsap.to(particles, 2, { timeScale: 0.5, ease: Power1.easeIn });
+gsap.to(particles, 2, { timeScale: 0.4, ease: Power1.easeIn });
 var currentTimeScale = gsap.globalTimeline.timeScale();
 var img = document.querySelectorAll('.wrapper img');
 img.forEach(function (e) {
@@ -197,7 +127,7 @@ img.forEach(function (e) {
         caption2.textContent = `By ${authors[images.indexOf(e.src.split('/').pop())]}`;
     });
     e.addEventListener('mouseout', () => {
-        particles.timeScale(0.2);
+        particles.timeScale(0.4);
         caption2.textContent = 'Office Oblitas';
     });
     e.addEventListener('click', () => {
